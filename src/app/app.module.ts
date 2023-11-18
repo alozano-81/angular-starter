9b1b64cb93a1d252979fe1271e9ee990bece5731
Andres Lozano <yeisonandreslozano@gmail.com>
2023-11-17 20:22:20 -0500

    rev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { APP_BASE_HREF } from '@angular/common';
+import { AppRoutingModule, appRoutingProviders } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PagesRoutingModule } from './pages/pages-routing.module';
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule,
+    AppRoutingModule,
+    PagesRoutingModule,
+    PagesModule],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [appRoutingProviders,
+    { provide : APP_BASE_HREF, useValue: '/gestion'}],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
